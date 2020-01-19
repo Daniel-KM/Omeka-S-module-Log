79a442d26b7fb4351bec9555e0de37f0497ab929
Daniel Berthereau <Daniel.github@Berthereau.net>
2020-01-20 00:00:00 +0100

    Logged all fatal errors during jobs.

--- src/Job/DispatchStrategy/Synchronous.php
+++ src/Job/DispatchStrategy/Synchronous.php
@@ -3,37 +3,53 @@ namespace Log\Job\DispatchStrategy;
 
 use Doctrine\ORM\EntityManager;
 use Omeka\Entity\Job;
+use Zend\Log\Logger;
 
 class Synchronous extends \Omeka\Job\DispatchStrategy\Synchronous
 {
     /**
-     * Log status and message if job terminates with a fatal error
+     * Copy of parent method, but with psr message and full logging.
+     * Logger may be null in order to manage various versions of core.
      *
-     * @param Job $job
-     * @param EntityManager $entityManager
+     * @inheritdoc
      */
-    public function handleFatalError(Job $job, EntityManager $entityManager)
+    public function handleFatalError(Job $job, EntityManager $entityManager, Logger $logger = null)
     {
         $lastError = error_get_last();
-        $errors = [E_ERROR, E_USER_ERROR, E_RECOVERABLE_ERROR];
-        if ($lastError && in_array($lastError['type'], $errors)) {
-            /** @var \Zend\Log\LoggerInterface $logger */
-            $logger = $this->serviceLocator->get('Omeka\Logger');
-            $logger->err(
-                "Fatal error: {message}\nin {file} on line {line}", // @translate
-                [
-                    'message' => $lastError['message'],
-                    'file' => $lastError['file'],
-                    'line' => $lastError['line'],
-                ]
-            );
+        if ($lastError) {
+            if (is_null($logger)) {
+                $logger = $this->serviceLocator->get('Omeka\Logger');
+            }
 
-            $job->setStatus(Job::STATUS_ERROR);
+            $errors = [E_ERROR, E_USER_ERROR, E_RECOVERABLE_ERROR];
+            if (in_array($lastError['type'], $errors)) {
+                $logger->err(
+                    'Fatal error: {message}\nin {file} on line {line}', // @translate
+                    [
+                        'message' => $lastError['message'],
+                        'file' => $lastError['file'],
+                        'line' => $lastError['line'],
+                    ]
+                );
 
-            // Make sure we only flush this Job and nothing else
-            $entityManager->clear();
-            $entityManager->merge($job);
-            $entityManager->flush();
+                $job->setStatus(Job::STATUS_ERROR);
+
+                // Make sure we only flush this Job and nothing else
+                $entityManager->clear();
+                $entityManager->merge($job);
+                $entityManager->flush();
+            }
+            // Log other errors according to the config for severity.
+            else {
+                $logger->warn(
+                    'Warning: {message}\nin {file} on line {line}', // @translate
+                    [
+                        'message' => $lastError['message'],
+                        'file' => $lastError['file'],
+                        'line' => $lastError['line'],
+                    ]
+                );
+            }
         }
     }
 }
