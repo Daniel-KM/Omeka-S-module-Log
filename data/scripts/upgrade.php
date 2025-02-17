8f481274e5520ca715bc7e65026086d82c8b5623
Daniel Berthereau <Daniel.git@Berthereau.net>
2025-02-17 00:00:00 +0000

    Cleaned code.

--- data/scripts/upgrade.php
+++ data/scripts/upgrade.php
@@ -2,7 +2,7 @@
 
 namespace Log;
 
-use Omeka\Stdlib\Message;
+use Common\Stdlib\PsrMessage;
 
 /**
  * @var Module $this
@@ -11,21 +11,29 @@ use Omeka\Stdlib\Message;
  * @var string $oldVersion
  *
  * @var \Omeka\Api\Manager $api
+ * @var \Omeka\View\Helper\Url $url
+ * @var \Laminas\Log\Logger $logger
  * @var \Omeka\Settings\Settings $settings
+ * @var \Laminas\I18n\View\Helper\Translate $translate
  * @var \Doctrine\DBAL\Connection $connection
+ * @var \Laminas\Mvc\I18n\Translator $translator
  * @var \Doctrine\ORM\EntityManager $entityManager
+ * @var \Omeka\Settings\SiteSettings $siteSettings
  * @var \Omeka\Mvc\Controller\Plugin\Messenger $messenger
  */
 $plugins = $services->get('ControllerPluginManager');
 $api = $plugins->get('api');
+$logger = $services->get('Omeka\Logger');
 $settings = $services->get('Omeka\Settings');
 $translate = $plugins->get('translate');
+$translator = $services->get('MvcTranslator');
 $connection = $services->get('Omeka\Connection');
 $messenger = $plugins->get('messenger');
+$siteSettings = $services->get('Omeka\Settings\Site');
 $entityManager = $services->get('Omeka\EntityManager');
 
 if (!method_exists($this, 'checkModuleActiveVersion') || !$this->checkModuleActiveVersion('Common', '3.4.66')) {
-    $message = new Message(
+    $message = new \Omeka\Stdlib\Message(
         $translate('The module %1$s should be upgraded to version %2$s or later.'), // @translate
         'Common', '3.4.66'
     );
@@ -44,12 +52,12 @@ if (PHP_VERSION_ID >= 80200) {
 
 if (version_compare($oldVersion, '3.2.1', '<')) {
     $sqls = <<<'SQL'
-ALTER TABLE log DROP FOREIGN KEY FK_8F3F68C5A76ED395;
-DROP INDEX user_idx ON log;
-ALTER TABLE `log` CHANGE `user_id` `owner_id` int(11) NULL AFTER `id`;
-ALTER TABLE log ADD CONSTRAINT FK_8F3F68C57E3C61F9 FOREIGN KEY (owner_id) REFERENCES user (id) ON DELETE SET NULL;
-CREATE INDEX owner_idx ON log (owner_id);
-SQL;
+        ALTER TABLE log DROP FOREIGN KEY FK_8F3F68C5A76ED395;
+        DROP INDEX user_idx ON log;
+        ALTER TABLE `log` CHANGE `user_id` `owner_id` int(11) NULL AFTER `id`;
+        ALTER TABLE log ADD CONSTRAINT FK_8F3F68C57E3C61F9 FOREIGN KEY (owner_id) REFERENCES user (id) ON DELETE SET NULL;
+        CREATE INDEX owner_idx ON log (owner_id);
+        SQL;
     foreach (explode(";\n", $sqls) as $sql) {
         try {
             $connection->executeStatement($sql);
@@ -61,8 +69,8 @@ SQL;
 if (version_compare($oldVersion, '3.3.12.6', '<')) {
     // @link https://www.doctrine-project.org/projects/doctrine-dbal/en/2.6/reference/types.html#array-types
     $sql = <<<'SQL'
-ALTER TABLE `log` CHANGE `context` `context` LONGTEXT NOT NULL COMMENT '(DC2Type:json)';
-SQL;
+        ALTER TABLE `log` CHANGE `context` `context` LONGTEXT NOT NULL COMMENT '(DC2Type:json)';
+        SQL;
     try {
         $connection->executeStatement($sql);
     } catch (\Exception $e) {
@@ -72,16 +80,15 @@ SQL;
 if (version_compare($oldVersion, '3.4.16', '<')) {
     // Create index first to avoid issue on foreign keys.
     $sqls = <<<'SQL'
-CREATE INDEX IDX_8F3F68C57E3C61F9 ON `log` (`owner_id`);
-CREATE INDEX IDX_8F3F68C5BE04EA9 ON `log` (`job_id`);
-CREATE INDEX IDX_8F3F68C5AEA34913 ON `log` (`reference`);
-CREATE INDEX IDX_8F3F68C5F660D16B ON `log` (`severity`);
-
-DROP INDEX owner_idx ON `log`;
-DROP INDEX job_idx ON `log`;
-DROP INDEX reference_idx ON `log`;
-DROP INDEX severity_idx ON `log`;
-SQL;
+        CREATE INDEX IDX_8F3F68C57E3C61F9 ON `log` (`owner_id`);
+        CREATE INDEX IDX_8F3F68C5BE04EA9 ON `log` (`job_id`);
+        CREATE INDEX IDX_8F3F68C5AEA34913 ON `log` (`reference`);
+        CREATE INDEX IDX_8F3F68C5F660D16B ON `log` (`severity`);
+        DROP INDEX owner_idx ON `log`;
+        DROP INDEX job_idx ON `log`;
+        DROP INDEX reference_idx ON `log`;
+        DROP INDEX severity_idx ON `log`;
+        SQL;
     /*
     $sqls = array_filter(explode(";\n", $sqls));
     $connection->transactional(function($connection) use ($sqls) {
@@ -99,9 +106,9 @@ SQL;
 }
 
 if (version_compare($oldVersion, '3.4.18', '<')) {
-    $message = new Message(
-        $translate('Support of the third party service Sentry was moved to a separate module, %1$sLog Sentry%2$s.'), // @translate
-        '<a href="https://gitlab.com/Daniel-KM/Omeka-S-module-LogSentry" target="_blank" rel="noopener">', '</a>'
+    $message = new PsrMessage(
+        $translate('Support of the third party service Sentry was moved to a separate module, {link}Log Sentry{link_end}.'), // @translate
+        ['link' => '<a href="https://gitlab.com/Daniel-KM/Omeka-S-module-LogSentry" target="_blank" rel="noopener">', 'link_end' => '</a>']
     );
     $message->setEscapeHtml(false);
     $messenger->addWarning($message);
